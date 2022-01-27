a6a4389a096c4bbe09e2c05c4b6f0a74730cb5ec
marisasaunders <marisaksaunders@gmail.com>
2022-01-27 15:21:32 -0600

    commit tree 77937a955f078ff17a42f42883b3b58ce746ed3e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function errorFunc(error) {
+  document.body.innerHTML = error.message
+}
+
+function submitData(name, email) {
+  const configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": 'application/json',
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  };
+  return fetch('http://localhost:3000/users', configObject)
+  .then(response => response.json())
+  .then(data => document.body.innerHTML = data.id)
+  .catch(errorFunc)
+}
